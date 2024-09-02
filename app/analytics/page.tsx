20272595411796a4805e91cfd092ede0a75fb7a6
Domengo <dominicsengo@gmail.com>
2024-09-02 22:23:35 +0300

    fixed nav

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -4,7 +4,7 @@ import { fleetMilage } from "../../components/notification-const"
 
 export default function Analytics() {
   return (
-    <div className="p-8 flex justify-center items-center h-full w-full">
+    <div className="p-8 flex justify-center items-center h-screen w-full">
       <AreaChart data={fleetMilage} />
     </div>
   )
